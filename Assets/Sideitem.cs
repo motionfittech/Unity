4e3cad3ba184054b550561b619885177e95d4c8a
andrew-motionfit <andrew@motionfittech.com>
2021-06-21 13:41:49 -0400

    Revert "Major Update V2"
    This reverts commit 03d9944ac69695367252cc4b40726e339c3aa94a.

--- Assets/Sideitem.cs
+++ Assets/Sideitem.cs
@@ -5,12 +5,10 @@ public class Sideitem : MonoBehaviour
 {
     Toggle currentToggle;
     WorkoutManager WM;
-    FitCapTest FCT;
     private void Start()
     {
       currentToggle  = this.transform.GetChild(0).GetComponent<Toggle>();
         WM = GameObject.FindObjectOfType<WorkoutManager>();
-         FCT = GameObject.FindObjectOfType<FitCapTest>();
     }
     // Start is called before the first frame update
     public void onValueChanged()
@@ -20,7 +18,7 @@ public class Sideitem : MonoBehaviour
 
         if (!WM._Dowehaveanimation)
             WM._Dowehaveanimation = true;
-        FCT.exerciseString = this.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text;
+
         WM.CenterButtonExercisetxt.text = this.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text;
         //print(this.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text +  " "+ currentToggle.isOn);
     }
