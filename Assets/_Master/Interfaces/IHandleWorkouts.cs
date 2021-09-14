407a29875fd0de343beaabc0b28e10c5f09d7aa6
MohammedRehman19 <mrehmanb5@gmail.com>
2021-09-15 03:12:02 +0500

    Major Update v1

--- Assets/_Master/Interfaces/IHandleWorkouts.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-interface IHandleWorkouts
-{
-     void NewWorkout(WorkoutScriptableObject workout);
-    void StopWorkout(WorkoutScriptableObject workout);
-    void ReadUserData();
-}
